4c30cf77d38a774d87e06a657fa1c1b200989fb4
H-9-mn <nu24q61@nmamit.in>
2024-09-28 12:37:27 +0530

    New commit

--- src/app/utils/profiles.ts
+++ src/app/utils/profiles.ts
@@ -20,4 +20,9 @@ export const profiles: ProfileProps[] = [
     name: "Satwik R Prabhu",
     quote: "I know where you live 🌝",
   },
+  {
+    username: "H-9-mn",
+    name: "H Mahima Nayak",
+    quote: "...",
+  }
 ];
